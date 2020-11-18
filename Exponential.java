d72ffff390e79025e705cdcd9b31deb158ae2e4e
BenjaminMerritt <73952590+BenjaminMerritt@users.noreply.github.com>
2020-11-18 11:09:59 -0500

    Working on Exponential

--- Exponential.java
+++ Exponential.java
@@ -1,4 +1,25 @@
+import javax.swing.JOptionPane;
+
+import java.text.DecimalFormat;
+
 public class Exponential 
 {
-    
+    int base;
+    int power;
+    int answer;
+
+    public Exponential()
+    {
+        calculate();
+    }
+
+    public void calculate()
+    {
+        String input = JOptionPane.showInputDialog("***** Exponents Calculator *****" +
+                        "\n \nEnter the base number of the equation.");
+
+        int inputInt = Integer.parseInt(input);
+
+        
+    }
 }
