9c9799f2b84bd93ef7c808d89d1e051eba2063fc
BenjaminMerritt <73952590+BenjaminMerritt@users.noreply.github.com>
2020-11-19 10:18:08 -0500

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -3,44 +3,49 @@ import java.util.ArrayList;
 
 public class Addition 
 {
-    public static void main(String[] args)
-    {
-
         ArrayList<Integer> additives = new ArrayList<Integer>();
 
-        int choice;
-        int sum = 0;
-        int selectedState = 0;
-        Object[] possibleStates = { "Yes", "No"};
-
-        choice = Integer.parseInt(JOptionPane.showInputDialog("What's the 1st number you wish to add?"));
-
-        additives.add(choice);
-        
-        choice = Integer.parseInt(JOptionPane.showInputDialog("What's the 2nd number you wish to add?"));
-
-        additives.add(choice);
-
-        selectedState = JOptionPane.showOptionDialog(null, "Would you like to add another numeber?", "Addition Calculator", +
-                         JOptionPane.INFORMATION_MESSAGE, selectedState, null, possibleStates, possibleStates[0]);
-        
-        if(selectedState == 0)
+        public Addition()
         {
-            do
-            {
-                choice = Integer.parseInt(JOptionPane.showInputDialog("What is the next number you would like to add? "));
-                additives.add(choice);
+            
+        } 
 
-                selectedState = JOptionPane.showOptionDialog(null, "Would you like to add another number?", "Addition Calculator", +
-                         JOptionPane.DEFAULT_OPTION, selectedState, null, possibleStates, possibleStates[0]);
-            } while(selectedState == 0);
-        }
-       
-        for(int i = 0; i < additives.size(); i++)
+        public void calculate()
         {
-            sum += additives.get(i);
-        }
+            int choice = Integer.parseInt(JOptionPane.showInputDialog("What's the 1st number you wish to add?"));
+
+            additives.add(choice);
+            
+            choice = Integer.parseInt(JOptionPane.showInputDialog("What's the 2nd number you wish to add?"));
     
-        JOptionPane.showMessageDialog(null, "The sum of the numbers you have entered is " + sum);
-    }
+            additives.add(choice);
+    
+            int selectedState = 0;
+
+            Object[] possibleStates = { "Yes", "No"};
+
+            selectedState = JOptionPane.showOptionDialog(null, "Would you like to add another numeber?", "Addition Calculator", +
+                             JOptionPane.INFORMATION_MESSAGE, selectedState, null, possibleStates, possibleStates[0]);
+            
+            if(selectedState == 0)
+            {
+                do
+                {
+                    choice = Integer.parseInt(JOptionPane.showInputDialog("What is the next number you would like to add? "));
+                    additives.add(choice);
+    
+                    selectedState = JOptionPane.showOptionDialog(null, "Would you like to add another number?", "Addition Calculator", +
+                             JOptionPane.DEFAULT_OPTION, selectedState, null, possibleStates, possibleStates[0]);
+                } while(selectedState == 0);
+            }
+           
+            int sum = 0;
+
+            for(int i = 0; i < additives.size(); i++)
+            {
+                sum += additives.get(i);
+            }
+        
+            JOptionPane.showMessageDialog(null, "The sum of the numbers you have entered is " + sum);
+        }
 }
