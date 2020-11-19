dff4baf5e2a031c96731e1f97f05b4a0f1a5b741
RedTheGeek <gmanred007@gmail.com>
2020-11-19 11:20:57 -0500

    Update Subtraction.java

--- Subtraction.java
+++ Subtraction.java
@@ -2,5 +2,12 @@ import javax.swing.JOptionPane;
 import java.util.ArrayList;
 
 public class Subtraction {
-    
+    ArrayList<Double> subtrahends = new ArrayList<Double>();
+public Subtraction()
+{}
+public void calculate(){
+    double choice = Double.parsedouble(JOptionPane.showInputDialog("What do you want to subtract from?"));
+    subtrahends.add(choice);
+    choice = Double.parsedouble(JOptionPane.showInputDialog("What do you want to subtract by?"));
+}
 }
