bcf0316583d1a65146c96845f2a1af39062269f5
RedTheGeek <gmanred007@gmail.com>
2020-11-19 20:49:39 -0500

    Update Division.java

--- Division.java
+++ Division.java
@@ -12,36 +12,5 @@ public class Division{
 
     public void calculate()
     {
-        double choice = Double.parseDouble(JOptionPane.showInputDialog("What is the number you would like to divide?"));
-
-        divisors.add(choice);
-        
-        choice = Double.parseDouble(JOptionPane.showInputDialog("What is the number you would like to divide by?"));
-
-        divisors.add(choice);
-
-        int selectedState = 0;
-
-        Object[] possibleStates = { "Yes", "No"};
-
-        selectedState = JOptionPane.showOptionDialog(null, "Would you like to divide by another number?", "Division Calculator", +
-                         JOptionPane.INFORMATION_MESSAGE, selectedState, null, possibleStates, possibleStates[0]);
-        
-        if(selectedState == 0)
-        {
-            do
-            {
-                choice = Integer.parseInt(JOptionPane.showInputDialog("What is the next number you would like to divide by? "));
-                
-                divisors.add(choice);
-
-                selectedState = JOptionPane.showOptionDialog(null, "Would you like to divide by another number?", "Division Calculator", +
-                         JOptionPane.DEFAULT_OPTION, selectedState, null, possibleStates, possibleStates[0]);
-            
-                        } while(selectedState == 0);
-        }
-
-
-        
     }
 }
