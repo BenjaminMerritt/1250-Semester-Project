07627bb9a82652e41994408dc9c721eaf488ce5f
RedTheGeek <73952658+RedTheGeek@users.noreply.github.com>
2020-11-20 11:50:56 -0500

    Update Division.java

--- Division.java
+++ Division.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 public class Division{
     
     ArrayList<Double> divisors = new ArrayList<Double>();
-    double dividend;
-    double quotient;
-
 
     public Division()
     {
@@ -16,13 +13,13 @@ public class Division{
     public void calculate()
     {
 
-        String input = JOptionPane.showInputDialog("What do you want to subtract from?");
+        String input = JOptionPane.showInputDialog("What do you want to divide?");
 
         dividend = Double.parseDouble(input);
 
-        input = JOptionPane.showInputDialog("What value would you like to subtract from the first?");
+        input = JOptionPane.showInputDialog("What is the number you would like to divide by?");
 
-        double divisor = Double.parseDouble(input);
+        double divisors = Double.parseDouble(input);
     
         divisors.add(divisor);
 
@@ -30,20 +27,20 @@ public class Division{
 
         Object[] possibleStates = { "Yes", "No"};
 
-        selectedState = JOptionPane.showOptionDialog(null, "Would you like to subtract another number?", "Subtraction Calculator", +
+        selectedState = JOptionPane.showOptionDialog(null, "Would you like to divide by another number?", "Division Calculator", +
                         JOptionPane.INFORMATION_MESSAGE, selectedState, null, possibleStates, possibleStates[0]);
     
         if(selectedState == 0)
         {
             do
             {
-                input = JOptionPane.showInputDialog("What is the next number you would like to Subtract? ");
+                input = JOptionPane.showInputDialog("What is the next number you would like to divide by? ");
 
                 divisor = Double.parseDouble(input);
                 
                 divisors.add(divisor);
 
-                selectedState = JOptionPane.showOptionDialog(null, "Would you like to Subtract another number?", "Subtraction Calculator", +
+                selectedState = JOptionPane.showOptionDialog(null, "Would you like to divide by another number?", "Division Calculator", +
                         JOptionPane.DEFAULT_OPTION, selectedState, null, possibleStates, possibleStates[0]);
             } while(selectedState == 0);
 
@@ -58,24 +55,22 @@ public class Division{
         }
 
         JOptionPane.showMessageDialog(null, toString());
-
-        divisors.clear();
+        
     }
     public String toString()
     {
-        String msg = "***** Division Calculation *****";
+        String msg = "***** Division Calculator *****";
 
         msg += "\nDividend: " + dividend;
 
         msg += "\nDivisor(s): ";
 
-        for(int i = 0; i < divisors.size(); i++)
+        for(int i =0; i < divisors.size(); i++)
         {
-            msg += divisors.get(i) + " ";
+            msg += divisors.get(i) + ", ";
         }
 
         msg += "\nQuotient: " + quotient;
 
         return msg;
-    }
-}
+
