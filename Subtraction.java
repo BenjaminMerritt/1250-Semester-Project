6b3cd7f7e74534c2a0f68876f720057286349f75
BenjaminMerritt <73952590+BenjaminMerritt@users.noreply.github.com>
2020-11-19 20:55:11 -0500

    Update Subtraction.java

--- Subtraction.java
+++ Subtraction.java
@@ -1,45 +1,80 @@
 import javax.swing.JOptionPane;
 import java.util.ArrayList;
 
-public class Subtraction {
+public class Subtraction 
+{
+
+    double minuend;
+    double difference;
        
-        ArrayList<Double> subtrahends = new ArrayList<Double>();
+    ArrayList<Double> subtrahends = new ArrayList<Double>();
 
-        public Subtraction(){   
+    public Subtraction()
+    {
 
-        }
+    }
 
-        public void calculate()
-
-        {       
-    
+    public void calculate()
+    {
         String input = JOptionPane.showInputDialog("What do you want to subtract from?");
+
+        minuend = Double.parseDouble(input);
+
+        input = JOptionPane.showInputDialog("What value would you like to subtract from the first?");
+
+        double subtrahend = Double.parseDouble(input);
     
-        input = Double.parseDouble(JOptionPane.showInputDialog("What do you want to subtract by?"));
-    
-        subtrahends.add(input);
+        subtrahends.add(subtrahend);
 
         int selectedState = 0;
 
         Object[] possibleStates = { "Yes", "No"};
 
         selectedState = JOptionPane.showOptionDialog(null, "Would you like to subtract another number?", "Subtraction Calculator", +
-                     JOptionPane.INFORMATION_MESSAGE, selectedState, null, possibleStates, possibleStates[0]);
+                        JOptionPane.INFORMATION_MESSAGE, selectedState, null, possibleStates, possibleStates[0]);
     
-        if(selectedState == 0){
-            do{
-                input = Double.parseDouble(JOptionPane.showInputDialog("What is the next number you would like to Subtract? "));
+        if(selectedState == 0)
+        {
+            do
+            {
+                input = JOptionPane.showInputDialog("What is the next number you would like to Subtract? ");
+
+                subtrahend = Double.parseDouble(input);
                 
-                subtrahends.add(input);
+                subtrahends.add(subtrahend);
 
                 selectedState = JOptionPane.showOptionDialog(null, "Would you like to Subtract another number?", "Subtraction Calculator", +
                         JOptionPane.DEFAULT_OPTION, selectedState, null, possibleStates, possibleStates[0]);
             } while(selectedState == 0);
-            }
 
-            int difference = 0;
-
-            for(int i = 0; i < )
-
+            
         } 
+        
+        difference = minuend;
+
+        for(int i = 0; i < subtrahends.size(); i++)
+        {
+            minuend = minuend - subtrahends.get(i);
+        }
+
+        JOptionPane.showMessageDialog(null, toString());
+    }
+
+    public String toString()
+    {
+        String msg = "***** Subtraction Calculation *****";
+
+        msg += "\nMinuend: " + minuend;
+
+        msg += "\nSubtrahend(s): ";
+
+        for(int i =0; i < subtrahends.size(); i++)
+        {
+            msg += subtrahends.get(i) + ", ";
+        }
+
+        msg += "\nDifference: " + difference;
+
+        return msg;
+    }
 }
